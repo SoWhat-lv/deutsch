1f4c68a1f647f84d5145acdfaa8a01808e044c6e
Karl <karlis.baumanis@icis.com>
2015-10-02 10:19:17 +0200

    Small reformating

--- Articles.php
+++ Articles.php
@@ -1,5 +1,17 @@
 <?php
 
-if(plural) {
-  artikel = die
+class WordOrder {
+
+	const plural  = "Noun that refers to multiple objects.";
+	const artikel = "one of three words that refer to gender of Noun";
+
+	function __construct() {
+
+	}
+
+	function init() {
+		if ( plural ) {
+			artikel = "die";
+		}
+	}
 }
